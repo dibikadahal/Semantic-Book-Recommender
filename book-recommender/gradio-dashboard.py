aee01dd357b6494d7fe834f660af417145fb1521
dibikadahal <dibikadahal@gmail.com>
2026-03-01 09:52:25 +0545

    same codes from vector search

--- book-recommender/gradio-dashboard.py
+++ book-recommender/gradio-dashboard.py
@@ -6,6 +6,7 @@ from langchain_community.document_loaders import TextLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 
 import gradio as gr
@@ -19,4 +20,21 @@ books["large_thumbnail"] = np.where(
     books["large_thumbnail"].isna(),
     "cover-not-found.jpg",
     books["large_thumbnail"]
+)
+
+raw_documents = TextLoader("tagged_description.txt", encoding="utf-8").load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=800,      # Sweet spot for book descriptions
+    chunk_overlap=0,
+    separators=["\n\n", "\n", " "]  # Try double newline first
+)
+
+documents = text_splitter.split_documents(raw_documents)
+
+embedding_model = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
+db_books = Chroma.from_documents(
+    documents,
+    embedding=embedding_model,
+    collection_name="books_fixed"
 )
